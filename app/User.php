a8312020e8653cf3a678572b5cd13c15d42da98b
fel_gaete <felipe.a.gaete.t@gmail.com>
2017-10-20 15:35:50 -0300

    Auth control

--- app/User.php
+++ app/User.php
@@ -10,12 +10,18 @@ class User extends Authenticatable
     use Notifiable;
 
     /**
+    * Model table
+    */
+    protected $table = 'usuarios';
+
+
+    /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'email', 'password',
     ];
 
     /**
