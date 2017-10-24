2e059fc5dfca674c38f48d393b42e62562cf0957
fel_gaete <felipe.a.gaete.t@gmail.com>
2017-10-24 15:29:07 -0300

    Add email confirmation view

---
+++ resources/views/email/verification.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Confirmaci¨®n de correo</title>
+  </head>
+  <body>
+    <a href="{{ action('Auth\RegisterController@confirm', [id => $user->id, 'token' => $user->confirmation_code]) }}">Confirmar correo</a>
+  </body>
+</html>
