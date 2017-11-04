7496f90895c738167fe222fd93ec7b6958563a32
felgaete <felipe.a.gaete.t@gmail.com>
2017-11-04 17:03:44 +0000

    Actualizar layout a bootstrap 4

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,99 +8,90 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title') - Votaconsciente</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
+        <nav class="navbar navbar-expand-md navbar-dark bg-primary bd-navbar">
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{ config('app.name', 'Laravel') }}
+            </a>
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggler collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav mr-auto">
+                    &nbsp;
+                </ul>
+                <!-- Right Side Of Navbar -->
+                <ul class="navbar-nav">
+                    <!-- Authentication Links -->
+                    @guest
+                    <li><a href="{{ route('login') }}">Ingresar</a></li>
+                    <li><a href="{{ route('register') }}">Registrarse</a></li>
+                    @else
+                    <li class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
 
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Ingresar</a></li>
-                            <li><a href="{{ route('register') }}">Registrarse</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        <ul class="dropdown-menu" role="menu">
+                            @if(!Auth::user()->votante)
+                            <li class="dropdown-item">
+                                <a href="{{ route('habilitar') }}">Habilitar voto</a>
+                            </li>
+                            @endif
+                            @if(Auth::user()->is_admin)
+                            <li class="dropdown-item">
+                                <a href="{{route('admin-index')}}">Administracion</a>
+                            </li>
+                            @endif
+                            <li class="dropdown-item">
+                                <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                                Salir
                                 </a>
 
-                                <ul class="dropdown-menu" role="menu">
-                                    @if(!Auth::user()->votante)
-                                    <li>
-                                      <a href="{{ route('habilitar') }}">Habilitar voto</a>
-                                    </li>
-                                    @endif
-                                    @if(Auth::user()->is_admin)
-                                    <li>
-                                      <a href="{{route('admin-index')}}">Administracion</a>
-                                    </li>
-                                    @endif
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Salir
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    {{ csrf_field() }}
+                                </form>
                             </li>
-                        @endguest
-                    </ul>
-                </div>
+                        </ul>
+                    </li>
+                    @endguest
+                </ul>
             </div>
         </nav>
         @auth
-        @if(!Auth::user()->votante)
-        <div class="container">
-          <div class="row justify-content-center">
-            <div class="col-md-8 col-md-offset-2">
-              <div class="alert alert-warning " role="alert">
-                <h4 class="alert-heading">Atención!</h4>
-                <p>
-                  <span class="glyphicon glyphicon-alert" aria-hidden="true"></span>
-                  <span class="sr-only"></span>
-                  Para permitir ingresar tu voto, primero tienes que habilitarte como votante,
-                  puedes hacerlo desde <a class="alert-link" href="{{route('habilitar')}}">aquí</a>.
-                </p>
-              </div>
+        <main>
+            @if(!Auth::user()->votante)
+            <div class="container mt-1">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="alert alert-warning " role="alert">
+                            <h4 class="alert-heading">Atención!</h4>
+                            <p>
+                                <span class="glyphicon glyphicon-alert" aria-hidden="true"></span>
+                                <span class="sr-only"></span>
+                                Para permitir ingresar tu voto, primero tienes que habilitarte como votante,
+                                puedes hacerlo desde <a class="alert-link" href="{{route('habilitar')}}">aquí</a>.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-        @endif
-        @endauth
-
-        @yield('content')
+            @endif
+            @endauth
+            @yield('content')
+        </main>
     </div>
 
     <!-- Scripts -->
