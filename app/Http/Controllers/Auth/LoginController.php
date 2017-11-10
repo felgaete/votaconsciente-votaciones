9929dfa80b54d30c9e3c7255526b491db6660403
Felipe Gaete Toro <felipe.a.gaete.t@gmail.com>
2017-11-10 12:50:47 -0300

    Redireccionar al inicio al loguearse

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/votar';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
